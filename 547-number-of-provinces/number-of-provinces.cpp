a0063031bae2b0481c4042e1cf85a7b9035ba402
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-07-27 16:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (47.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void dfs ( unordered_map<int ,bool>&visited, int i, unordered_map<int, vector<int> >&um){
+    void dfs ( unordered_map<int ,bool>&visited, int i, vector<vector<int>>& isConnected){
         visited[i]= true;
 
-        for( auto j: um[i]){
-            if( !visited[j]){
-                dfs( visited,j,um);
+        for( int j = 0 ;j<isConnected.size();j++){
+            if( isConnected[i][j] && !visited[j]){
+                dfs( visited,j,isConnected);
             }
         }
     }
@@ -17,23 +17,24 @@ public:
 
       
         int count=0;
-        unordered_map<int, vector<int> >um;
+    
 
-        for( int i =0 ;i< n;i++){
-            for( int j =0;j<m;j++){
-                 if ( isConnected[i][j] ){
-                    um[i].push_back(j);
-                 }
-            }
-        }
+        
         unordered_map<int,bool>visited;
 
-        for( auto i :um){
-            
-            if( !visited[i.first]){
-                dfs(visited,i.first,um );
+        for( int i =0; i<isConnected.size();i++){
+            if( !visited[i]){
+                visited[i]= true;
+                for( int j =0;j<isConnected.size();j++){
+                if( isConnected[i][j] && !visited[j]){
+                    dfs( visited,j,isConnected);
+                }
+                
+            }
                 count++;
             }
+            
+            
         }
 
 return count;
