59161983bac66f6b7cca5814919f409b455f1376
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-07-28 23:49:46 +0530

    Time: 6 ms (13.53%) | Memory: 16.5 MB (12.84%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve( int left, int right, int n , vector<string> &ans, string st){
+
+        if ( right==n ) { 
+            ans.push_back(st );
+            return ;
+        }
+
+        if( left<n && left>= right ){
+            string x = st + '(';
+            solve( left+1,right, n ,ans, x);
+        }
+        if( left>right){
+            string y = st+')';
+            solve( left, right+1, n ,ans, y);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string st="";
+        solve( 0,0,n,ans,st);
+        return ans;
+
+
+        
+    }
+};
