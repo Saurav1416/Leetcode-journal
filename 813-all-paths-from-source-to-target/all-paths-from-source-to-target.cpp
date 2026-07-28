196dc6fb3fe896e529c0bcba0e2bd6b6b62f7b49
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-07-28 22:38:14 +0530

    Time: 12 ms (23.73%) | Memory: 19.4 MB (19.35%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void dfs( vector<vector<int>> &anss,vector<int>ans, vector<bool>& visited,
+    void dfs( vector<vector<int>> &anss,vector<int>ans,
     vector<vector<int>>& graph,int node,int tgt ){
-
-        if( node == tgt) {
-            ans.push_back(node);
-            anss.push_back(ans);
-            ans.pop_back();
-            return ;
-        }
-        visited[node]= true;
         ans.push_back(node);
-
-        for( auto i : graph[node]){
-            if( !visited[i]){
-                dfs(anss,ans,visited,graph,i,tgt);
-            }
+        if( node == tgt) {
+            
+            anss.push_back(ans);
+            
+           
         }
-        visited[node]= false;
+       
+        
+
+       else {for( auto i : graph[node]){
+            
+                dfs(anss,ans,graph,i,tgt);
+            
+        }}
+        
         ans.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
 
      vector<vector<int>> anss;
-     vector<bool>visited( graph.size(),false);
+     
      vector<int>ans;
-     dfs(anss, ans ,visited,graph,0,graph.size()-1);
+     dfs(anss, ans ,graph,0,graph.size()-1);
      return anss;
         
     }
