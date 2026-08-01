73500a9063e6b28aa85d8267edd7c99dda08595b
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-08-01 22:14:18 +0530

    Time: 14 ms (53.46%) | Memory: 18.8 MB (99.15%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -18,29 +18,46 @@ public:
 
 class Solution {
 public:
+    void solve( Node * root, int ind, Node * parent){
+        if( !root) return;
+
+        if( parent && ind ==0){
+            root->next = parent->right;
+        }
+        else if(parent && parent->next && ind == 1){
+            root->next = parent->next->left;
+        }
+
+        solve( root->left , 0 ,root);
+        solve( root->right,1,root);
+    }
     Node* connect(Node* root) {
 
-        if( !root) return root;
+         if( !root) return root;
 
-        queue<Node* > q;
+        // queue<Node* > q;
 
-        q.push( root);
+        // q.push( root);
 
-        while( !q.empty()){
-            int size = q.size();
+        // while( !q.empty()){
+        //     int size = q.size();
 
-            for( int i =0;i< size;i++){
+        //     for( int i =0;i< size;i++){
 
-                Node * a = q.front();
-                q.pop();
-                if( a->left) q.push( a->left);
-                if(a->right) q.push( a->right);
-                if( i ==size-1){ continue;}
-                a->next = q.front ();
+        //         Node * a = q.front();
+        //         q.pop();
+        //         if( a->left) q.push( a->left);
+        //         if(a->right) q.push( a->right);
+        //         if( i ==size-1){ continue;}
+        //         a->next = q.front ();
                 
-            }
+        //     }
 
-        }
+        // }
+        // return root;
+
+
+        solve( root, -1,nullptr);
         return root;
         
     }
