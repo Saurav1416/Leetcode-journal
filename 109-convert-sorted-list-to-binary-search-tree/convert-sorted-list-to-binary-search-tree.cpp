a76d2b1ccb0f0e7e5c3c8dc1c8cede1524d57076
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-08-04 14:57:19 +0530

    Time: 4 ms (67.23%) | Memory: 33.3 MB (20.21%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findmid( ListNode * start, ListNode * end){
+
+        if( !start  || start ==end){
+            return nullptr;
+        }
+
+        if( start ->next==end ){
+           return start;
+        }
+        ListNode * slow = start;
+       ListNode *  fast = start;
+
+        while( fast!=end  && fast->next !=end){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+       
+        return slow;
+
+    }
+    TreeNode * solve( ListNode * start, ListNode * end){
+
+        if ( !start  || start ==end){
+            return nullptr;
+        }
+        if(start && (start ->next ==end )){
+            TreeNode * startt =new TreeNode ( start->val);
+            return startt;
+        }
+
+        ListNode* a = findmid( start,end);
+        
+         TreeNode * b = new TreeNode ( a->val);
+        b->left = solve( start, a);
+        b->right = solve( a->next , end);
+
+        return b;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+
+        return solve( head, nullptr);
+
+        
+    }
+};
