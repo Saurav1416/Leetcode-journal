374224a554e1880fe6d9db4bfc3ddbc840bf7c62
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-08-21 15:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (54.33%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
+
 
         
-        int n = nums.size();
-        if( n ==1) return 0;
-        vector<int> dp ( nums.size(),-1);
+        // int n = nums.size();
+        // if( n ==1) return 0;
+        // vector<int> dp ( nums.size(),-1);
 
-        dp[n-1]= 0;
+        // dp[n-1]= 0;
+        // int minn =INT_MAX/2;
 
-        for( int i = n-2;i>=0;i--){
+        // for( int i = n-2;i>=0;i--){
 
-            int minn =INT_MAX/2;
-            for( int j = i+1;j<= i+nums[i] && j<n;j++){
+            
+        //     for( int j = i+1;j<= i+nums[i] && j<n;j++){
 
-                minn = min( minn , dp[j]);
+        //         minn = min( minn , dp[j]);
+        //     }
+        //     dp[i]= minn+1;
+        //     minn = INT_MAX/2;
+        // }
+        // return dp [0];
+
+
+    int jump(std::vector<int>& nums) {
+        // Fast I/O micro-optimization
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+
+        int jumps = 0;
+        int current_end = 0;
+        int farthest = 0;
+
+        // Iterate up to nums.size() - 1 because we don't need to jump once at the last index
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = std::max(farthest, i + nums[i]);
+
+            // Reached the end of the current jump coverage range
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
             }
-            dp[i]= minn+1;
         }
-        return dp [0];
-        
+
+        return jumps;
     }
 };
+        
