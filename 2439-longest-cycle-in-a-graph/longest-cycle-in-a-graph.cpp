52661ac6ef507d099a985ada46e4c2d71cf822c8
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-08-31 22:42:19 +0530

    Time: 10 ms (97.61%) | Memory: 94.6 MB (99.06%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
-    void solve( unordered_map<int,vector<int>> &adj,vector<int> &v,vector<int> &dfsv,int &ans,int n ){
-
-
-        dfsv[n] = 1;
-        for( auto i: adj[n]){
-            if( v[i]==-1){
-                
-                v[i] = v[n]+1;
-                solve( adj,v,dfsv,ans,i);
-            }
-            else if(dfsv[i]== 1 ){
-                int y = v[n]-v[i]+1;
-                ans = max(ans,y);
-            }
-        }
-        dfsv[n]= -1;
-
-    }
     int longestCycle(vector<int>& edges) {
+        int time = 0, ans = -1, n = edges.size();
+        vector<int> vis(n, 0);
 
-        int n = edges.size();
-        unordered_map<int,vector<int>>adj;
+        for(int i=0; i<n; i++) {
+            if(vis[i]) continue;
 
-        for( int i =0;i<n;i++){
+            int next = i;
+            int start = time;
+            while(next != -1 && vis[next] == 0) {
+                vis[next] = time++;
+                next = edges[next];
+            }
 
-            if( edges[i]==-1) continue;
-
-            adj[i].push_back(edges[i]);
+            // if we find a cycle (vis[next] != 0)
+            if(next != -1 && vis[next] >= start) {
+                ans = max(ans, time - vis[next]);
+            }
         }
-        vector<int> v( n,-1);
-        
-        vector<int>dfsv( n,-1);
-        vector<int>parent;
-        int ans= -1;
-        for( int i =0;i<n;i++){
-                if( v[i]==-1){
-                    v[i]=1;
-                    solve( adj,v,dfsv,ans,i);
-                }
-        }
+
         return ans;
-        
-
-      
-
-        
     }
 };
