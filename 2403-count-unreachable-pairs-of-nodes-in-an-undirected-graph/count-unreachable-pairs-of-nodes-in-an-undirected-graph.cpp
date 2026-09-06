345569e50395ced3783ebf463d0d7472552380de
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-06 16:58:55 +0530

    Time: 11 ms (95.52%) | Memory: 142.5 MB (88.85%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,55 +1,44 @@
 class Solution {
-public:
-
-    int findp( int x, vector<int>&parent){
-
-        if(parent[x]!=x ){
-           return parent[x]= findp(parent[x],parent);
+    int findp(int x, vector<int>& parent) {
+        if (parent[x] != x) {
+            return parent[x] = findp(parent[x], parent);
         }
         return x;
     }
 
-    void unionn(int a, int b , vector<int>&parent,vector<long long>&size){
-        int x = findp(a,parent);
-        int y = findp( b, parent);
-        if( x == y ) return ;
+    void unionn(int a, int b, vector<int>& parent, vector<long long>& size) {
+        int x = findp(a, parent);
+        int y = findp(b, parent);
+        if (x == y) return;
 
-        if( x>y){
-            swap( x,y);
-        }
-        size[y]+= size[x];
-
-        parent[x]= y;
-        return ;
+        // Union by size
+        if (size[x] < size[y]) swap(x, y);
+        size[x] += size[y];
+        parent[y] = x;
     }
+
+public:
     long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n);
+        vector<long long> size(n, 1);
+        for (int i = 0; i < n; i++) parent[i] = i;
 
-        vector<int> parent( n ,-1);
-        for( int i =0;i<n;i++){
-            parent[i] =i;
+        for (const auto& edge : edges) {
+            unionn(edge[0], edge[1], parent, size);
         }
 
-        vector<long long > size( n ,1);
-        unordered_map<int,int>visited;
-        long long ans=0;
-        int numb = n;
+        long long ans = 0;
+        long long remainingNodes = n;
 
-        for( int i =0;i< edges.size();i++){
-           unionn( edges[i][1],edges[i][0],parent,size);
+        for (int i = 0; i < n; i++) {
+            // Only process component roots  
+            if (parent[i] == i) {
+                long long componentSize = size[i];
+                remainingNodes -= componentSize;
+                ans += componentSize * remainingNodes;
+            }
         }
 
-        for( int i =0;i<n;i++){
-            int t = findp( parent[i],parent);
-            if( !visited[t])
-            {visited[t] =1;
-             long long c = size[t];
-            numb-= c;
-            ans+= (numb*c);}
-
-        }
         return ans;
-        
-
-        
     }
 };
