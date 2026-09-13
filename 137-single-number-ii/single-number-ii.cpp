0e6404ce00381ed356fec929165bce525ae1007c
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-13 15:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (99.16%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -3,20 +3,32 @@ public:
     int singleNumber(vector<int>& nums) {
 
        
-        int ans =0;
+        // int ans =0;
 
-        for( int  i =0;i< 32 ;i++){
-            int c =0;
-            int t = 1<<i;
-            for( int j =0;j<nums.size();j++){
+        // for( int  i =0;i< 32 ;i++){
+        //     int c =0;
+        //     int t = 1<<i;
+        //     for( int j =0;j<nums.size();j++){
 
-                 if( nums[j] & t) c++;
-            }
-            if( c%3!=0){
-              ans = ans | t;
-            }
+        //          if( nums[j] & t) c++;
+        //     }
+        //     if( c%3!=0){
+        //       ans = ans | t;
+        //     }
+        // }
+        // return ans;    code in nlog is faster than this 
+
+
+        int ones = 0;
+        int twos = 0;
+
+        for( int i =0;i<nums.size();i++){
+
+            ones = (ones ^ nums[i])  & ~twos;
+            twos = (twos ^ nums[i] ) & ~ones;
         }
-        return ans;
+        return ones;
+
         
     }
 
