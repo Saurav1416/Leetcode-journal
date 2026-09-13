3cc2d63f61347936470089bda16d1c26010116f8
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-13 21:33:01 +0530

    Time: 15 ms (28.79%) | Memory: 17.1 MB (26.84%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        unordered_set<string> set( wordDict.begin(),wordDict.end());
-        vector<bool>dp ( n+1, false);
-        dp[0]= true ;  // we are storing each elt in one index ahead 
+    bool solve( int index , string &s, vector<int> & dp,unordered_set<string>& set ){
 
-        for( int i =1;i<=n;i++){
+        if( index ==s.size()) return true;
 
-            for( int j =0;j<i; j++){
+        if(dp[index]!=-1) return dp[index];
 
-                if( dp[j]){  // are we are stroing each elt one index ahead so here we are talking about previous index 
-                    string sub = s.substr( j,i-j);  //here j represent curr index on string 
-                    if( set.count(sub)){
-                        dp[i]= true;
-                        break;
-                    }
-                }
+        for( int j =index ;j<s.size();j++){
+            string sub = s.substr( index, j-index+1);
+            if( set.count(sub) && solve( j+1,s,dp ,set ) ){
+                return dp[index] = 1;
             }
         }
-        return dp [n];
+        return dp[index]=0;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // int n = s.size();
+        // unordered_set<string> set( wordDict.begin(),wordDict.end());
+        // vector<bool>dp ( n+1, false);
+        // dp[0]= true ;  // we are storing each elt in one index ahead 
+
+        // for( int i =1;i<=n;i++){
+
+        //     for( int j =0;j<i; j++){
+
+        //         if( dp[j]){  // are we are stroing each elt one index ahead so here we are talking about previous index 
+        //             string sub = s.substr( j,i-j);  //here j represent curr index on string 
+        //             if( set.count(sub)){
+        //                 dp[i]= true;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        // return dp [n];
+
+        int n = s.size();
+        unordered_set<string> set( wordDict.begin(),wordDict.end());
+        vector<int>dp ( n+1, -1 );
+        return solve( 0, s, dp,set);
     }
 };
