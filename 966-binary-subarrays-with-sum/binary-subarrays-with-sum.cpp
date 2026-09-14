948384c744c05f2c2dccad60c91d8036036dfc35
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-14 23:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (98.54%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
+
+    int atmost(vector<int>&nums, int x){
+
+        if( x <0)  return 0;
+
+        int l =0;
+        int sum =0;
+        int ans = 0;
+        for( int i =0;i< nums.size();i++){
+
+            sum+= nums[i];
+
+            while( sum >x){
+                sum-=nums[l];
+                l++;
+                
+            }
+            ans+=i-l+1;
+
+        }
+        return ans;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-       unordered_map<int,int> mp ;
-       mp[0]=1;
-       int sum =0;
-       int ans=0;
-       for( int i =0;i< nums.size();i++){
+    //    unordered_map<int,int> mp ;
+    //    mp[0]=1;
+    //    int sum =0;
+    //    int ans=0;
+    //    for( int i =0;i< nums.size();i++){
 
-          sum += nums[i];
+    //       sum += nums[i];
 
-          if( mp.find( sum-goal)!=mp.end()){
-              ans+=mp[sum-goal];
-          }
-            mp[sum]++;
-          }
+    //       if( mp.find( sum-goal)!=mp.end()){
+    //           ans+=mp[sum-goal];
+    //       }
+    //         mp[sum]++;
+    //       }
        
-       return ans;
+    //    return ans;
+
+    //advance way 
+
+    return atmost( nums, goal) - atmost( nums, goal-1);
         
     }
 };
