d6338bbe830528c2653ce63f615dff4d1183654f
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-15 11:28:54 +0530

    Time: 2 ms (21.86%) | Memory: 11.2 MB (6.91%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve( string s , vector<int>&dp, int i){
+        if ( i ==s.size()) return 1;
+        if( s[i]=='0') return 0;
+        if(dp[i]!=-1) return dp[i];
+        
+        int a = solve( s, dp, i+1);
+        int b =0;
+        if( s[i]=='1' ||s[i]=='2'){
+            if ( i+1<s.size() && stoi(s.substr( i,2))<27){
+              b=  solve( s,dp,i+2);
+            }
+        }
+        return dp[i] = a+b;
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.size(),-1);
+
+        return solve( s, dp,0);
+
+        
+    }
+};
