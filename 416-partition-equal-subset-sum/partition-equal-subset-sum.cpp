eebc0847c6d84498439c66dc962fe8390350b163
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-22 22:45:04 +0530

    Time: 79 ms (83.02%) | Memory: 13.5 MB (81.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int  solve( int i , int t , int sum , int &n, vector<vector<int>>&dp,
-    vector<int>&nums){
+    // int  solve( int i , int t , int sum , int &n, vector<vector<int>>&dp,
+    // vector<int>&nums){
 
-        if( t == sum) return dp[i][t]= 1;
-        if( i>=n) return 0;
+    //     if( t == sum) return dp[i][t]= 1;
+    //     if( i>=n) return 0;
 
-        if( dp[i][t]!=-1) return dp[i][t];
+    //     if( dp[i][t]!=-1) return dp[i][t];
 
-        //skip
-        bool a =false;
-        bool b = false;
-        a = solve( i+1, t,sum,n,dp,nums);
-        b = solve( i +1, t+nums[i], sum-nums[i],n,dp,nums) ;
+    //     //skip
+    //     bool a =false;
+    //     bool b = false;
+    //     a = solve( i+1, t,sum,n,dp,nums);
+    //     b = solve( i +1, t+nums[i], sum-nums[i],n,dp,nums) ;
 
-        return dp[i][t]= (a |b);
-    }
+    //     return dp[i][t]= (a |b);
+    // }
+
+
+    // this approachs works but better memoization would be if we only look up to sum /2 cuz to partition in 2 set both sum has to be totalsum/2
+
     bool canPartition(vector<int>& nums) {
         int sum =0; 
         int n = nums.size();
@@ -23,8 +27,20 @@ public:
 
             sum+= nums[i];
         }
-        vector<vector<int>>dp( n+1,vector<int>( sum+1,-1));
-        return solve( 0,0, sum ,n,dp,nums);
+        if( sum %2!=0) return false;
+
+        // vector<vector<int>>dp( n+1,vector<int>( sum+1,-1));
+        vector<bool >dp ( sum/2 +1, false);
+        dp[0] = true;
+
+        for( int i :nums){
+            for( int j = sum/2;j>=i;j--){
+
+                dp[j] = dp[j]|| dp[j-i];
+            }
+        }
+        return dp[sum/2];
+        
         
     }
 };
