edc32fd3011b50c8ec5f1dabd9c8adb46a10f619
Saurav <137406336+Saurav1416@users.noreply.github.com>
2026-09-22 23:42:43 +0530

    Time: 4 ms (94.43%) | Memory: 10.1 MB (96.29%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        
+
+        vector<unsigned int> dp ( amount+1,0);
+        dp[0]=1;
+
+        for( int i :coins){
+
+            for( int j =i;j<=amount;j++){
+                dp[j]+= dp[j-i];
+            }
+        }
+        return dp[amount];
+        
+    }
+};
